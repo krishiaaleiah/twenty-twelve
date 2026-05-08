da88af05104a4e715a478656f4b49fb8f7659ef2
Krystine Kaye Nequiota <nequiotakrystinebulaag@gmail.com>
2026-05-09 04:02:42 +0800

    Added navigation scenes and scripts (d_001_001 - d_002_014)

---
+++ d_002_012.gd
@@ -0,0 +1,24 @@
+extends Control
+
+func _ready() -> void:
+	CursorManager.set_normal()
+
+func _on_forward_mouse_entered() -> void:
+	CursorManager.set_hover()
+func _on_forward_mouse_exited() -> void:
+	CursorManager.set_normal()
+func _on_forward_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton:
+		if event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+			AudioManager.play_sfx("click")
+			get_tree().change_scene_to_file("res://Scenes/Game/d_002_013.tscn")
+
+func _on_back_mouse_entered() -> void:
+	CursorManager.set_back()
+func _on_back_mouse_exited() -> void:
+	CursorManager.set_normal()
+func _on_back_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton:
+		if event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+			AudioManager.play_sfx("click")
+			get_tree().change_scene_to_file("res://Scenes/Game/d_002_011.tscn")
