ca1fa0194afe4e9d3301ac7e2d1916376c83f186
Krishia Aleiah C. Mojica <krishiamojica23@gmail.com>
2026-05-11 00:59:59 +0800

    f26 to f37

---
+++ Scenes/Scripts/f_027_01.gd
@@ -0,0 +1,32 @@
+extends Control
+func _ready() -> void:
+	CursorManager.set_normal()
+
+func _on_forward_mouse_entered() -> void:
+	CursorManager.set_hover()
+
+
+func _on_forward_mouse_exited() -> void:
+	CursorManager.set_normal()
+
+
+func _on_forward_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton:
+		if event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+			AudioManager.play_sfx("click")
+			get_tree().change_scene_to_file("res://Scenes/Game/f_026_01.tscn")
+
+
+func _on_backward_mouse_entered() -> void:
+	CursorManager.set_back()
+
+
+func _on_backward_mouse_exited() -> void:
+	CursorManager.set_normal()
+
+
+func _on_backward_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton:
+		if event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+			AudioManager.play_sfx("click")
+			get_tree().change_scene_to_file("res://Scenes/Game/f_026_01.tscn")
--- Scenes/Scripts/f_036_01.gd
+++ Scenes/Scripts/f_036_01.gd
@@ -1,24 +1,32 @@
 extends Control
-
 func _ready() -> void:
 	CursorManager.set_normal()
 
 func _on_forward_mouse_entered() -> void:
 	CursorManager.set_hover()
+
 func _on_forward_mouse_exited() -> void:
 	CursorManager.set_normal()
+
+
 func _on_forward_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event is InputEventMouseButton:
 		if event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
 			AudioManager.play_sfx("click")
-			get_tree().change_scene_to_file("res://Scenes/Game/d_002_013.tscn")
+			get_tree().change_scene_to_file("res://Scenes/Game/f_037_01.tscn")
 
-func _on_back_mouse_entered() -> void:
+
+
+func _on_backward_mouse_entered() -> void:
 	CursorManager.set_back()
-func _on_back_mouse_exited() -> void:
+
+
+func _on_backward_mouse_exited() -> void:
 	CursorManager.set_normal()
-func _on_back_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+
+
+func _on_backward_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event is InputEventMouseButton:
 		if event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
 			AudioManager.play_sfx("click")
-			get_tree().change_scene_to_file("res://Scenes/Game/d_002_011.tscn")
+			get_tree().change_scene_to_file("res://Scenes/Game/f_035_01.tscn")
